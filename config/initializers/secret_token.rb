667d175daa2ed081d1e973a70b3a216019b007a0
boxl <todd@bocksell.com>
2014-01-02 11:07:05 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiamondApp::Application.config.secret_key_base = '761739454250450f987a9eb28d001512b11e5d4a481f6a11f14967933c4dd0f7346f86dc425316405f19ba3842c19c0ae106379412cfad630b38cedd5696b5fa'
